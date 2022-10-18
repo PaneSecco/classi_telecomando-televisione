f990c95af7e0133c381ba218afdd8ff14211ecc8
Gatto lunatico <101564199+PaneSecco@users.noreply.github.com>
2022-10-18 20:32:41 +0200

    Aggiungere i file di progetto.

---
+++ classi_telecomando-televisione/telecomando.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classi_telecomando_televisione
+{
+    internal class telecomando
+    {
+        televisione tv = new televisione();
+
+        //immutabili
+        private string produttore;
+        private string modello;
+        private string modalita;
+
+        public telecomando()
+        {
+
+        }
+
+        public void setcanale(int cambio)
+        {
+            tv.setcanale(cambio);
+        }
+
+        public int getcanale()
+        {
+            return tv.getcanale();
+        }
+
+        public void cambio_stato()
+        {
+            tv.cambio_stato();
+        }
+
+        public bool get_stato()
+        {
+            return tv.get_stato();
+        }
+
+        public void volume_su()
+        {
+            tv.volume_su();
+        }
+        public void volume_giu()
+        {
+            tv.volume_giu();
+        }
+
+        public int get_volume()
+        {
+            return tv.get_volume();
+        }
+        public void set_specifiche(string produttore1, string modello1, string modalita1)
+        {
+            produttore = produttore1;
+            modello = modello1;
+            modalita = modalita1;
+        }
+        public string[] get_specifiche()
+        {
+            string[] array = { produttore, modello, modalita };
+            return array;
+        }
+    }
+}
---
+++ classi_telecomando-televisione/televisione.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classi_telecomando_televisione
+{
+    internal class televisione
+    {
+        //mutabili
+        private int canale = 0;
+        private int volume = 0;
+        private bool stato = false;
+
+        public televisione()
+        {
+
+        }
+        public void setcanale(int cambio)
+        {
+            canale = cambio;
+        }
+        public int getcanale()
+        {
+            return canale;
+        }
+        public void cambio_stato()
+        {
+            if (stato == false)
+            {
+                stato = true;
+            }
+            else
+            {
+                stato = false;
+            }
+        }
+        public bool get_stato()
+        {
+            return stato;
+        }
+        public void volume_su()
+        {
+            volume = volume + 1;
+        }
+        public void volume_giu()
+        {
+            volume = volume - 1;
+        }
+
+        public int get_volume()
+        {
+            return volume;
+        }
+
+    }
+}
